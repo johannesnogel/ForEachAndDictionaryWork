d68ff44249d71838db36d3da2b686f0770f2d424
opilane <opilane@HP-CZC0359NN6>
2023-02-21 09:35:10 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var domains = new Dictionary<string, string>
+            {
+                {"PJK", "Püha Johannese Kool"},
+                {"VG", "Viimsi Gümnaasium"},
+                {"TTHK", "Tallinna Tööstusharidus Keskus"},
+            };
+
+            int i = 1;
+            foreach (var pair in domains)
+            {
+                Console.WriteLine($"{pair.Value}");
+                i++;
+            }
+        }
+    }
+}
